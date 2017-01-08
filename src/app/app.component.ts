0a760c9f3cd8c729265abe94ea51c9d21bd6ae85
wikibootup <wikibootup@gmail.com>
2017-01-08 19:29:42 +0900

    apply simple form

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>
+      <app-simple-form></app-simple-form>
+  </div>`
+})
+export class AppComponent {
+  title = 'app works!';
+}
